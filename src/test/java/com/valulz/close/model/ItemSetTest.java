33742f28aba907c690e0f79620e20a931a331252
Valentin <vdesportes@gmail.com>
2016-03-04 10:48:48 +0100

    test(ItemSet) : constructor with parameter initialize a set fill with given items

--- src/test/java/com/valulz/close/model/ItemSetTest.java
+++ src/test/java/com/valulz/close/model/ItemSetTest.java
@@ -2,6 +2,9 @@ package com.valulz.close.model;
 
 import org.junit.Test;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ItemSetTest {
@@ -16,4 +19,22 @@ public class ItemSetTest {
         //Then
         assertThat(itemSet.getItemSet().isEmpty()).isTrue();
     }
+
+    @Test
+    public void constructor_with_parameter_initialize_a_set_of_item_fill_with_given_items() throws Exception {
+        //Given
+        Item a = new Item("a"); Item b = new Item("b");
+
+        SortedSet<Item> expected = new TreeSet<>();
+        expected.add(a);
+        expected.add(b);
+
+        //When
+        ItemSet itemSet = new ItemSet(a, b);
+
+        //Then
+        assertThat(itemSet.getItemSet()).isEqualTo(expected);
+    }
+
+
 }
