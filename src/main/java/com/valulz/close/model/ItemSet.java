8fdebf6e4146c4c30a669412ef42c95c80ac1278
Valentin <vdesportes@gmail.com>
2016-03-05 20:14:56 +0100

    update(ItemSet) : Change from a delegation approach to an inheritance approach
    - ItemSet is now a subclass of TreeSet<Item>
    - I change it for practical reason (mostly because, it was pointless to, add every sigle method of the TreeSet class + compareTo, instead of just doing an inheritance)

--- src/main/java/com/valulz/close/model/ItemSet.java
+++ src/main/java/com/valulz/close/model/ItemSet.java
@@ -1,76 +1,16 @@
 package com.valulz.close.model;
 
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.SortedSet;
 import java.util.TreeSet;
 
-public class ItemSet implements Comparable<ItemSet>{
-
-    private SortedSet<Item> itemSet;
-
-    public ItemSet() {
-        this.itemSet = new TreeSet<>();
-    }
+public class ItemSet extends TreeSet<Item> implements Comparable<ItemSet>{
 
     public ItemSet(Item... items){
-        itemSet = new TreeSet<>();
-        Collections.addAll(itemSet, items);
-    }
+        super();
 
-    public ItemSet(ItemSet itemSet){
-        this.itemSet = new TreeSet<>(itemSet.getItemSet());
-    }
-
-    public int size(){
-        return itemSet.size();
-    }
-
-    public boolean isEmpty(){
-        return itemSet.isEmpty();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ItemSet itemSet1 = (ItemSet) o;
-
-        return itemSet.equals(itemSet1.itemSet);
-
-    }
-
-    @Override
-    public int hashCode() {
-        return itemSet.hashCode();
-    }
-
-    //Devrais-je y avoir accès ?
-    public SortedSet<Item> getItemSet() {
-        return itemSet;
-    }
-
-    public boolean add(Item item){
-        if(item == null){
-            throw new IllegalArgumentException("Given item cannot be null");
+        for (Item item : items) {
+            add(item);
         }
-
-        return itemSet.add(item);
-    }
-
-    public boolean contains(Item item){
-        if(item == null){
-            throw new IllegalArgumentException("The given Item cannot be null");
-        }
-        return itemSet.contains(item);
-    }
-
-    public boolean retainAll(ItemSet o){
-        if(o == null){
-            throw new IllegalArgumentException("The given ItemSet cannot be null");
-        }
-        return itemSet.retainAll(o.itemSet);
     }
 
     @Override
@@ -83,8 +23,8 @@ public class ItemSet implements Comparable<ItemSet>{
             return this.size() < o.size() ? -1 : 1;
         }
 
-        Iterator<Item> itThis = this.itemSet.iterator();
-        Iterator<Item> itOther = o.itemSet.iterator();
+        Iterator<Item> itThis = this.iterator();
+        Iterator<Item> itOther = o.iterator();
 
         int compareToItem = 0;
 
--- src/test/java/com/valulz/close/model/ItemSetTest.java
+++ src/test/java/com/valulz/close/model/ItemSetTest.java
@@ -2,276 +2,23 @@ package com.valulz.close.model;
 
 import org.junit.Test;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
 public class ItemSetTest {
 
     @Test
-    public void empty_constructor_initialize_an_empty_set_of_item() throws Exception {
+    public void constructor_with_multiple_items_parameter_create_an_new_set_filled_with_those_items() throws Exception {
         //Given
-
-        //When
-        ItemSet itemSet = new ItemSet();
-
-        //Then
-        assertThat(itemSet.getItemSet().isEmpty()).isTrue();
-    }
-
-    @Test
-    public void constructor_with_parameter_initialize_a_set_of_item_fill_with_given_items() throws Exception {
-        //Given
-        Item a = new Item("a"); Item b = new Item("b");
-
-        SortedSet<Item> expected = new TreeSet<>();
-        expected.add(a);
-        expected.add(b);
+        Item a=new Item("a");
+        Item b=new Item("b");
 
         //When
         ItemSet itemSet = new ItemSet(a, b);
 
         //Then
-        assertThat(itemSet.getItemSet()).isEqualTo(expected);
-    }
+        assertThat((Iterable<? extends Item>) itemSet).contains(a,b);
 
-    @Test
-    public void constructor_with_ItemSet_parameter_make_a_copy_of_the_given_parameter() throws Exception {
-        //Given
-        Item a = new Item("a"); Item b = new Item("b");
-        ItemSet expected = new ItemSet(a, b);
-
-        //When
-        ItemSet itemSet = new ItemSet(expected);
-
-        //Then
-        assertThat(itemSet).isEqualTo(expected);
-    }
-
-    @Test
-    public void size_return_the_number_of_item_inside_the_set() throws Exception {
-        //Given
-        Item a = new Item("a"); Item b = new Item("b");
-
-        //When
-        ItemSet itemSet = new ItemSet(a, b);
-
-        //Then
-        assertThat(itemSet.size()).isEqualTo(2);
-    }
-
-    @Test
-    public void isEmpty_return_true_if_the_set_contains_no_item() throws Exception {
-        //Given
-        ItemSet itemSet = new ItemSet();
-
-        //When
-        final boolean isEmpty = itemSet.isEmpty();
-
-        //Then
-        assertThat(isEmpty).isTrue();
-    }
-
-    @Test
-    public void isEmpty_return_false_if_the_set_contains_at_least_one_item() throws Exception {
-        //Given
-        ItemSet itemSet = new ItemSet(new Item("a"));
-
-        //When
-        final boolean isEmpty = itemSet.isEmpty();
-
-        //Then
-        assertThat(isEmpty).isFalse();
-    }
-
-    @Test
-    public void hashCode_return_same_number_if_two_ItemSet_are_equal() throws Exception {
-        //Given
-        ItemSet i1 = new ItemSet(new Item("a"));
-        ItemSet i2 = new ItemSet(new Item("a"));
-
-        //When
-        final boolean isSameHashCode = i1.hashCode() == i2.hashCode();
-
-        //Then
-        assertThat(isSameHashCode).isTrue();
-        assertThat(i1).isEqualTo(i2);
-    }
-
-    @Test
-    public void hasCode_two_empty_ItemSet_return_the_same_hashCode() throws Exception {
-        //Given
-        ItemSet i1 = new ItemSet();
-        ItemSet i2 = new ItemSet();
-
-        //When
-        final boolean isSameHashCode = i1.hashCode() == i2.hashCode();
-
-        //Then
-        assertThat(isSameHashCode).isTrue();
-        assertThat(i1.isEmpty()).isTrue();
-        assertThat(i2.isEmpty()).isTrue();
-    }
-
-    @Test
-    public void hasCode_return_2_different_number_if_two_ItemSet_are_different() throws Exception {
-        //Given
-        ItemSet i1 = new ItemSet(new Item("a"));
-        ItemSet i2 = new ItemSet();
-
-        //When
-        final boolean isSameHashCode = i1.hashCode() == i2.hashCode();
-
-        //Then
-        assertThat(isSameHashCode).isFalse();
-        assertThat(i1).isNotEqualTo(i2);
-    }
-
-    @Test
-    public void add_fail_if_the_given_item_is_null() throws Exception {
-        //Given
-        ItemSet set = new ItemSet();
-
-        try{
-            //When
-            set.add(null);
-            fail("The ItemSet.add method should have failed because the given Item was null");
-        } catch (IllegalArgumentException ex){
-            assertThat(ex.getMessage()).isEqualTo("Given item cannot be null");
-        }
-    }
-
-    @Test
-    public void add_return_true_if_the_given_item_is_added_to_the_set() throws Exception {
-        //Given
-        Item a = new Item("a");
-        ItemSet itemSet = new ItemSet();
-
-        //When
-        final boolean isAdded = itemSet.add(a);
-
-        //Then
-        assertThat(isAdded).isTrue();
-        assertThat(itemSet.contains(a)).isTrue();
-    }
-
-    @Test
-    public void contains_return_false_if_the_given_item_is_not_present_inside_the_ItemSet() throws Exception {
-        //Given
-        Item a = new Item("a");
-        ItemSet itemSet = new ItemSet();
-
-        //When
-        final boolean isPresent = itemSet.contains(a);
-
-        //Then
-        assertThat(isPresent).isFalse();
-    }
-
-    @Test
-    public void contains_return_true_if_the_given_item_is_already_in_the_ItemSet() throws Exception {
-        //Given
-        Item a = new Item("a");
-        ItemSet itemSet = new ItemSet(a);
-
-        //When
-        final boolean isPresent = itemSet.contains(a);
-
-        //Then
-        assertThat(isPresent).isTrue();
-    }
-
-    @Test
-    public void contains_fail_if_the_given_item_is_null() throws Exception {
-        //Given
-        ItemSet itemSet = new ItemSet();
-
-        try{
-            itemSet.contains(null);
-            fail("contains should have thrown an IllegalArgumentException because of the null parameter");
-        }catch (IllegalArgumentException ex){
-            assertThat(ex.getMessage()).isEqualTo("The given Item cannot be null");
-        }
-    }
-
-    @Test
-    public void retainAll_fail_if_the_given_parameter_is_null() throws Exception {
-        //Given
-        ItemSet itemSet = new ItemSet();
-
-        try{
-            itemSet.retainAll(null);
-            fail("retainAll should have thrown an IllegalArgumentException because of the null parameter");
-        }catch (IllegalArgumentException ex){
-            assertThat(ex.getMessage()).isEqualTo("The given ItemSet cannot be null");
-        }
-    }
-
-    @Test
-    public void retainAll_returns_true_if_the_ItemSet_had_changed() throws Exception {
-        //Given
-        Item a=new Item("a");Item b=new Item("b");Item c=new Item("c");
-
-        ItemSet itemSet = new ItemSet(a, b);
-        ItemSet changer = new ItemSet(a, c);
-        ItemSet expected = new ItemSet(a);
-
-        //When
-        final boolean hasChanged = itemSet.retainAll(changer);
-
-        //Then
-        assertThat(itemSet).isEqualTo(expected);
-        assertThat(hasChanged).isTrue();
-    }
-
-    @Test
-    public void retainAll_return_false_if_the_ItemSet_had_not_changed() throws Exception {
-        //Given
-        Item a=new Item("a");Item b=new Item("b");Item c=new Item("c");
-
-        ItemSet itemSet = new ItemSet(a);
-        ItemSet changer = new ItemSet(a);
-        ItemSet expected = new ItemSet(a);
-
-        //When
-        final boolean hasChanged = itemSet.retainAll(changer);
-
-        //Then
-        assertThat(itemSet).isEqualTo(expected);
-        assertThat(hasChanged).isFalse();
-    }
-
-    @Test
-    public void if_the_attribute_ItemSet_and_the_given_one_have_nothing_in_common_retainAll_set_the_attribute_to_empty() throws Exception {
-        //Given
-        Item a=new Item("a");Item b=new Item("b");
-
-        ItemSet itemSet = new ItemSet(a);
-        ItemSet changer = new ItemSet(b);
-
-        //When
-        final boolean hasChanged = itemSet.retainAll(changer);
-
-        //Then
-        assertThat(hasChanged).isTrue();
-        assertThat(itemSet.isEmpty()).isTrue();
-    }
-
-    @Test
-    public void add_return_false_if_the_given_item_was_already_present_in_the_ItemSet() throws Exception {
-        //Given
-        Item a = new Item("a");
-        ItemSet itemSet = new ItemSet(a);
-
-        //When
-        final boolean isPresent = itemSet.contains(a);
-        final boolean isAdded = itemSet.add(a);
-
-        //Then
-        assertThat(isPresent).isTrue();
-        assertThat(isAdded).isFalse();
     }
 
     @Test
@@ -290,7 +37,8 @@ public class ItemSetTest {
     @Test
     public void compareTo_return_a_negative_value_if_the_given_set_is_larger_than_the_attribute_one() throws Exception {
         //Given
-        ItemSet itemSet = new ItemSet(new Item("a"));
+        ItemSet itemSet;
+        itemSet = new ItemSet(new Item("a"));
         ItemSet compare = new ItemSet(new Item("a"), new Item("b"));
 
         //When
@@ -303,7 +51,8 @@ public class ItemSetTest {
     @Test
     public void compareTo_return_a_positive_value_if_the_given_set_is_smaller_than_the_attribute_one() throws Exception {
         //Given
-        ItemSet itemSet = new ItemSet(new Item("a"), new Item("b"));
+        ItemSet itemSet;
+        itemSet = new ItemSet(new Item("a"), new Item("b"));
         ItemSet compare = new ItemSet(new Item("a"));
 
         //When
@@ -324,13 +73,14 @@ public class ItemSetTest {
 
         //Then
         assertThat(compareTo).isEqualTo(0);
-        assertThat(itemSet).isEqualTo(compare);
+        assertThat((Iterable<? extends Item>) itemSet).isEqualTo(compare);
     }
 
     @Test
     public void compareTo_return_a_positive_value_if_this_is_lexicographically_greater_than_the_given_one() throws Exception {
         //Given
-        ItemSet itemSet = new ItemSet(new Item("a"), new Item("b"), new Item("z"));
+        ItemSet itemSet;
+        itemSet = new ItemSet(new Item("a"), new Item("b"), new Item("z"));
         ItemSet compare = new ItemSet(new Item("a"), new Item("b"), new Item("d"));
 
         //When
@@ -343,7 +93,8 @@ public class ItemSetTest {
     @Test
     public void compareTo_return_a_negative_value_if_this_is_lexicographically_lesser_than_the_given_one() throws Exception {
         //Given
-        ItemSet itemSet = new ItemSet(new Item("a"), new Item("b"), new Item("d"));
+        ItemSet itemSet;
+        itemSet = new ItemSet(new Item("a"), new Item("b"), new Item("d"));
         ItemSet compare = new ItemSet(new Item("a"), new Item("b"), new Item("z"));
 
         //When
